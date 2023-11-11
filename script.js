4246f869cc351b521c942e43837843eb0812e056
sydrob <55166840+sydrobhez@users.noreply.github.com>
2023-11-10 20:33:04 -0600

    adding functionality

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const number = document.querySelector('.number');
+const generateBtn = document.querySelector('.generate');
 
+const generateNumber = () => {
+  // Number between 0 - 10
+  const randomNumber = Math.floor(Math.random() * 1000);
+  // console.log(randomNumber);
+  number.innerHTML = randomNumber;
+};
+
+// Event Listeners
+generateBtn.addEventListener('click', generateNumber);
